e4566274e2ac46d954f18c1d9386c7f201addce6
Raider992 <19stargazer92@gmail.com>
2020-06-21 14:14:40 +0300

    Дз ко второму уроку

--- task1.py
+++ task1.py
@@ -1,10 +1,1 @@
-# 1. Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
-
-num = int(input("Введите трёхзначное число"))
-
-first_num = num % 10
-sec_num = num % 100 // 10
-th_num = num // 100
-
-print("Сумма: ", first_num + sec_num + th_num)
-print("Произведение: ", first_num * sec_num * th_num)
+123
