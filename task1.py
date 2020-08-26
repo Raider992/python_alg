ecc652499d477862f465f69142bd4e2d8d4febed
Raider992 <19stargazer92@gmail.com>
2020-08-26 13:54:12 +0300

    Моё дз к восьмому уроку

--- task1.py
+++ task1.py
@@ -1,98 +1,14 @@
 """
-1. Отсортируйте по убыванию методом "пузырька" одномерный целочисленный массив,
-заданный случайными числами на промежутке [-100; 100). Выведите на экран
-исходный и отсортированный массивы. Сортировка должна быть реализована в
-виде функции. Обязательно доработайте алгоритм (сделайте его умнее).
+Задание 1.
+Реализуйте кодирование строки "по Хаффману".
+У вас два пути:
+1) тема идет тяжело? тогда вы можете, опираясь на пример с урока, сделать свою версию алгоритма
+Разрешается и приветствуется изменение имен переменных, выбор других коллекций, различные изменения
+и оптимизации.
+КОПИПАСТ ПРИМЕРА ПРИНИМАТЬСЯ НЕ БУДЕТ!
+2) тема понятна? постарайтесь сделать свою реализацию.
+Вы можете реализовать задачу, например, через ООП или предложить иной подход к решению.
 
-Идея доработки: если за проход по списку не совершается ни одной сортировки,
-то завершение
-Обязательно сделайте замеры времени обеих реализаций
-и обосновать дала ли оптимизация эффективность
-
-Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
-а по убыванию
+ВНИМАНИЕ: примеры заданий будут размещены в последний день сдачи.
+Но постарайтесь обойтись без них.
 """
-import timeit
-from random import randint
-import copy
-
-
-orig_list = [randint(-100,100) for _ in range(20)]
-orig_list2 = copy.deepcopy(orig_list) # Глубокое копирование, чтобы не сортировать
-                                      # один и тот же список
-
-orig_list_large = [randint(-100,100) for _ in range(5000)]
-orig_list_large2 = copy.deepcopy(orig_list_large)
-
-print(orig_list)
-print(orig_list2, '\n')
-
-def reverse_bubble(li):
-    lst = copy.copy(li)
-    n = len(lst)
-    while(n > 1):
-        for j in range(len(lst) - 1, 0, -1):
-            if lst[j] > lst[j-1]:
-                lst[j], lst[j-1] = lst[j-1], lst[j]
-        n -= 1
-    return lst
-
-
-def reverse_bubble_opt(li):
-    lst = copy.copy(li)
-    update = True
-    n = len(lst)
-    while(update and n > 1):
-        update = False
-        for j in range(len(lst) - 1, 0, -1):
-            if lst[j] > lst[j-1]:
-                lst[j], lst[j-1] = lst[j-1], lst[j]
-                update = True
-    return lst
-
-
-sorted_list = reverse_bubble(orig_list)
-sorted_list2 = reverse_bubble_opt(orig_list2)
-
-print(sorted_list)
-print(sorted_list2, '\n')
-
-print('Измерения времени выполнения:\n')
-
-print('Для маленьких списков:')
-
-print('Не оптимизированная: ', timeit.timeit("reverse_bubble(orig_list)", \
-    setup="from __main__ import reverse_bubble, orig_list", number=10))
-
-print('Оптимизированная: ', timeit.timeit("reverse_bubble_opt(orig_list2)", \
-    setup="from __main__ import reverse_bubble_opt, orig_list2", number=10), '\n')
-
-print('Для больших списков:')
-
-print('Не оптимизированная: ', timeit.timeit("reverse_bubble(orig_list_large)", \
-    setup="from __main__ import reverse_bubble, orig_list_large", number=10))
-
-print('Оптимизированная', timeit.timeit("reverse_bubble_opt(orig_list_large2)", \
-    setup="from __main__ import reverse_bubble_opt, orig_list_large2", number=10))
-
-'''
-[-34, -48, -80, -8, 89, -50, -41, 0, -18, -47, 1, 80, -71, 33, 13, -9, -34, -70, 24, -43]
-[-34, -48, -80, -8, 89, -50, -41, 0, -18, -47, 1, 80, -71, 33, 13, -9, -34, -70, 24, -43] 
-
-[89, 80, 33, 24, 13, 1, 0, -8, -9, -18, -34, -34, -41, -43, -47, -48, -50, -70, -71, -80]
-[89, 80, 33, 24, 13, 1, 0, -8, -9, -18, -34, -34, -41, -43, -47, -48, -50, -70, -71, -80] 
-
-Измерения времени выполнения:
-
-Для маленьких списков:
-Не оптимизированная:  0.0009263000000000049
-Оптимизированная:  0.0007169999999999954 
-
-Для больших списков:
-Не оптимизированная:  27.370464600000002
-Оптимизированная 26.80808
-
-Process finished with exit code 0
-
-
-'''
--- task2.py
+++ task2.py
@@ -1,95 +1,79 @@
 """
-2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
-заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
-и отсортированный массивы.
+Задание 2.**
 
-Пример:
-Введите число элементов: 5
-Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
-Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
+Доработайте пример структуры "дерево",
+рассмотренный на уроке.
+
+Предложите варианты доработки и оптимизации
+(например, валидация значений узлов в соответствии с требованиями для бинарного дерева)
+
+Поработайте с доработанной структурой, позапускайте на реальных данных.
 """
-from copy import copy
-from timeit import timeit
-import numpy as np
 
-def merge_sort(li):
+class BinaryTree:
+    def __init__(self, root_obj):
+        # корень
+        self.root = root_obj
+        # левый потомок
+        self.left_child = None
+        # правый потомок
+        self.right_child = None
 
-    try:
-        assert len(li) > 1
-    except AssertionError:
-        print('В массиве должно быть больше 1 элемента')
-        quit()
+    # добавить левого потомка
+    def insert_left(self, new_node):
+        # если у узла нет левого потомка
+        if self.left_child == None:
+            # тогда узел просто вставляется в дерево
+            # формируется новое поддерево
+            self.left_child = BinaryTree(new_node)
+        # если у узла есть левый потомок
+        else:
+            # тогда вставляем новый узел
+            tree_obj = BinaryTree(new_node)
+            # и спускаем имеющегося потомка на один уровень ниже
+            tree_obj.left_child = self.left_child
+            self.left_child = tree_obj
 
-    lst = copy(li)
+    # добавить правого потомка
+    def insert_right(self, new_node):
+        # если у узла нет правого потомка
+        if self.right_child == None:
+            # тогда узел просто вставляется в дерево
+            # формируется новое поддерево
+            self.right_child = BinaryTree(new_node)
+        # если у узла есть правый потомок
+        else:
+            # тогда вставляем новый узел
+            tree_obj = BinaryTree(new_node)
+            # и спускаем имеющегося потомка на один уровень ниже
+            tree_obj.right_child = self.right_child
+            self.right_child = tree_obj
 
-    def merge(lst):
+    # метод доступа к правому потомку
+    def get_right_child(self):
+        return self.right_child
 
-        if len(lst) > 1:
+    # метод доступа к левому потомку
+    def get_left_child(self):
+        return self.left_child
 
-            mid = len(lst) // 2
-            left = lst[:mid]
-            right = lst[mid:]
+    # метод установки корня
+    def set_root_val(self, obj):
+        self.root = obj
 
-            merge(left)
-            merge(right)
-
-            i = j = k = 0
-
-            while i < len(left) and j < len(right):
-                if left[i] < right[j]:
-                    lst[k] = left[i]
-                    i += 1
-                else:
-                    lst[k] = right[j]
-                    j += 1
-                k += 1
-
-            while i < len(left):
-                lst[k] = left[i]
-                i += 1
-                k += 1
-
-            while j < len(right):
-                lst[k] = right[j]
-                j += 1
-                k += 1
-
-    merge(lst)
-
-    return lst
-
-
-def gen_list_float(bottom, top, f, size):
-    return [float(format(np.random.uniform(bottom,top), f'.{f}f')) for _ in range(size)]
-
-
-def get_list_el():
-    n = int(input('Введите число элементов: '))
-    return gen_list_float(0,50,2,n)
+    # метод доступа к корню
+    def get_root_val(self):
+        return self.root
 
 
-li = get_list_el()
-
-li_sorted = merge_sort(li)
-
-print(f'Исходный массив: \n {li}')
-
-print(f'Отсортированный массив: \n {li_sorted}')
-
-print('\n')
-
-li1 = gen_list_float(0,50,2,10)
-
-print('Время работы сортировки 10 элементов: ', timeit("merge_sort(li1)",
-    setup="from __main__ import merge_sort, li1", number=1),)
-
-li2 = gen_list_float(0,50,2,100)
-
-print('Время работы сортировки 100 элементов: ', timeit("merge_sort(li2)",
-    setup="from __main__ import merge_sort, li2", number=1))
-
-li3 = gen_list_float(0,50,2,10000)
-
-print('Время работы сортировки 10000 элементов: ', timeit("merge_sort(li3)",
-    setup="from __main__ import merge_sort, li3", number=1))
-
+r = BinaryTree(8)
+print(r.get_root_val())
+print(r.get_left_child())
+r.insert_left(4)
+print(r.get_left_child())
+print(r.get_left_child().get_root_val())
+r.insert_right(12)
+print(r.get_right_child())
+print(r.get_right_child().get_root_val())
+r.get_right_child().set_root_val(16)
+print(r.get_right_child().get_root_val())
